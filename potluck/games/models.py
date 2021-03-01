ffe6b6e26ffde30a5e9ee620c059022861d2ad5b
Tibor Leupold <tibor.leupold@torchbox.com>
2021-02-28 15:34:03 -1000

    Add game model

--- potluck/games/models.py
+++ potluck/games/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from potluck.teams.models import Team
+
+
+# class GameTeam(models.Model):
+#     """Through model to connect a game and a team."""
+
+#     pass
+
+
+class Game(models.Model):
+    teams = models.ManyToManyField(Team, related_name="+")
+    winner = models.ForeignKey(
+        Team, limit_choices_to=teams, on_delete=models.CASCADE, related_name="+"
+    )
