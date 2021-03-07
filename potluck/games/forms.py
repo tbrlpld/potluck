a9baa435f323dc0ebc33bd88b99c43324ea57e55
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-06 20:44:15 -1000

    Add validation for two teams input

--- potluck/games/forms.py
+++ potluck/games/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 from potluck.games.models import Game
 
 
@@ -7,3 +8,9 @@ class CreateGameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ("teams",)
+
+    def clean_teams(self):
+        data = self.cleaned_data["teams"]
+        if len(data) != 2:
+            raise forms.ValidationError("Please select two teams.")
+        return data
