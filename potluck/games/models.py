667accec860a672707f9c243ded7c8077cfffa9d
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-06 16:11:37 -1000

    Add winning team field

--- potluck/games/models.py
+++ potluck/games/models.py
@@ -11,6 +11,10 @@ from potluck.teams.models import Team
 
 class Game(models.Model):
     teams = models.ManyToManyField(Team, related_name="+")
-    winner = models.ForeignKey(
-        Team, limit_choices_to=teams, on_delete=models.CASCADE, related_name="+"
+    winning_team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+        related_name="+",
+        blank=True,
+        null=True,
     )
