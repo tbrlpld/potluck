de9119f17f225427bb733581494b856f3da46258
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-06 17:14:36 -1000

    Add create game form

---
+++ potluck/games/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from potluck.games.models import Game
+
+
+class CreateGameForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = ("teams",)
--- potluck/games/views.py
+++ potluck/games/views.py
@@ -1,16 +1,15 @@
 # from django.shortcuts import render
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 
+from potluck.games.forms import CreateGameForm
 from potluck.games.models import Game
 
 
 # Create your views here.
 class CreateGameView(CreateView):
     model = Game
-    fields = ("teams",)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = "Create Game"
-        return context
+    form_class = CreateGameForm
+    extra_context = {"title": "Create Game"}
+    success_url = reverse_lazy("home:home")
