32fe772d0e2756650938d4823df3b05c8f12fd2b
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-10 12:49:54 -1000

    Add pot create view

--- potluck/pots/urls.py
+++ potluck/pots/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 
-from potluck.pots.views import PotListView
+from potluck.pots.views import PotListView, PotCreateView
 
 app_name = "pots"
 
-urlpatterns = [path("", PotListView.as_view(), name="list")]
+urlpatterns = [
+    path("", PotListView.as_view(), name="list"),
+    path("create/", PotCreateView.as_view(), name="create"),
+]
--- potluck/pots/views.py
+++ potluck/pots/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from potluck.pots.models import Pot
@@ -6,3 +7,9 @@ from potluck.pots.models import Pot
 class PotListView(generic.ListView):
     model = Pot
     context_object_name = "pots"
+
+
+class PotCreateView(generic.CreateView):
+    model = Pot
+    success_url = reverse_lazy("pots:list")
+    fields = ("name",)
