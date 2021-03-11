bd2546fada706f9bf0f9c08f75346f0e66e8828a
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-10 14:06:48 -1000

    Add rough setup for game add view

--- potluck/pots/urls.py
+++ potluck/pots/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 
-from potluck.pots.views import PotListView, PotCreateView, PotDetailView
+from potluck.pots.views import PotListView, PotCreateView, PotDetailView, GameAddView
 
 app_name = "pots"
 
 urlpatterns = [
     path("", PotListView.as_view(), name="list"),
     path("<int:pk>/", PotDetailView.as_view(), name="detail"),
+    path("<int:pot_id>/add-game", GameAddView.as_view(), name="add_game"),
     path("create/", PotCreateView.as_view(), name="create"),
 ]
--- potluck/pots/views.py
+++ potluck/pots/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from potluck.pots.models import Pot
+from potluck.games.models import Game
 
 
 class PotListView(generic.ListView):
@@ -18,3 +19,20 @@ class PotCreateView(generic.CreateView):
     model = Pot
     success_url = reverse_lazy("pots:list")
     fields = ("name",)
+
+
+class GameAddView(generic.CreateView):
+    model = Game
+    success_url = reverse_lazy("pots:list")
+    fields = ("teams", "pot")
+    template_name = "pots/game_add.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pot_id"] = self.kwargs["pot_id"]
+        return context
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial["pot"] = self.kwargs["pot_id"]
+        return initial
