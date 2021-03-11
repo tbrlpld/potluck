ff363b9b348821b272cafaa8b9120c68e7109c03
Tibor Leupold <tibor.leupold@torchbox.com>
2021-03-10 15:41:45 -1000

    Fix changed view name

--- potluck/pots/urls.py
+++ potluck/pots/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 
-from potluck.pots.views import PotListView, PotCreateView, PotDetailView, GameAddView
+from potluck.pots.views import PotListView, PotCreateView, PotDetailView, PotAddGameView
 
 app_name = "pots"
 
 urlpatterns = [
     path("", PotListView.as_view(), name="list"),
     path("<int:pk>/", PotDetailView.as_view(), name="detail"),
-    path("<int:pot_id>/add-game", GameAddView.as_view(), name="add_game"),
+    path("<int:pot_id>/add-game", PotAddGameView.as_view(), name="add_game"),
     path("create/", PotCreateView.as_view(), name="create"),
 ]
