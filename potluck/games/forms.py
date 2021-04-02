db7d0df08c6efad9a9ae02e7f099b62e28b8581d
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-01 17:50:11 -1000

    Remove validation from form
    Now the tests are failing. In the next step I need to add the validator to the model

--- potluck/games/forms.py
+++ potluck/games/forms.py
@@ -8,9 +8,3 @@ class CreateGameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = ("teams",)
-
-    def clean_teams(self):
-        data = self.cleaned_data["teams"]
-        if len(data) != 2:
-            raise forms.ValidationError("Please select two teams.")
-        return data
