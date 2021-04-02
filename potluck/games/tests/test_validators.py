49305867ffdca563073e15c9b51d2f4ae60e37bd
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-01 17:23:51 -1000

    Add additional validation tests

--- potluck/games/tests/test_validators.py
+++ potluck/games/tests/test_validators.py
@@ -4,6 +4,20 @@ from pytest import raises
 from potluck.games.validators import validate_len
 
 
+def test_validate_len_empty_list_error():
+    with raises(ValidationError):
+        validate_len([])
+
+
 def test_validate_len_one_input_error():
     with raises(ValidationError):
         validate_len([1])
+
+
+def test_validate_len_three_inputs_error():
+    with raises(ValidationError):
+        validate_len([1, 2, 3])
+
+
+def test_validate_len_two_inputs_success():
+    assert validate_len([1, 2]) is None
