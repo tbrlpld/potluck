92ef61a6bb2063675a71b35a6855594f9b5eb283
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-02 18:01:02 -1000

    Limit teams for game pick to teams in game

--- potluck/picks/forms.py
+++ potluck/picks/forms.py
@@ -10,3 +10,12 @@ class CreateGamePickForm(forms.ModelForm):
         widgets = {
             "game": forms.HiddenInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        game = self.initial.get("game")
+        if game is None:
+            raise ValueError(
+                "{0} needs receive a game as initial data.".format(self.__class__)
+            )
+        self.fields["pick"].queryset = self.initial["game"].teams
--- potluck/picks/views.py
+++ potluck/picks/views.py
@@ -10,12 +10,17 @@ class CreateGamePickView(generic_views.CreateView):
     model = GamePick
     form_class = CreateGamePickForm
 
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
+        game_id = self.kwargs.get("game_id")
+        self.game = shortcuts.get_object_or_404(Game, pk=game_id)
+
     def get_initial(self):
         initial = super().get_initial()
         initial["game"] = self.game
         return initial
 
-    def setup(self, request, *args, **kwargs):
-        super().setup()
-        game_id = self.kwargs.get("game_id")
-        self.game = shortcuts.get_object_or_404(Game, pk=game_id)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["game"] = self.game
+        return context
