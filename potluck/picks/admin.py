22d15c7b2dea41e975b18aca89b87f8731000174
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-02 18:09:03 -1000

    Add game pick model admin

--- potluck/picks/admin.py
+++ potluck/picks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from potluck.picks.models import GamePick
+
 # Register your models here.
+@admin.register(GamePick)
+class GamePickAdmin(admin.ModelAdmin):
+    pass
