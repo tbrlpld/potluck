1b06f2dc20ba0757f06b5fa6e9d998f78be7bc09
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-03 22:21:32 -1000

    Add simple pot list view tests

--- potluck/pots/tests/test_views.py
+++ potluck/pots/tests/test_views.py
@@ -7,6 +7,27 @@ from potluck.pots.tests.factories import PotFactory
 
 
 @pytest.mark.django_db
+class TestPotListView:
+    def test_pot_list_empty(self):
+        url = reverse("pots_list")
+        client = Client()
+
+        response = client.get(url)
+
+        assert response.status_code == HTTPStatus.OK
+
+    def test_pot_list_contains_existing_pot(self):
+        pot = PotFactory.create()
+        url = reverse("pots_list")
+        client = Client()
+
+        response = client.get(url)
+
+        assert response.status_code == HTTPStatus.OK
+        assert pot.name in str(response.content)
+
+
+@pytest.mark.django_db
 class TestPotDetailView:
     def test_pot_name_in_detail_view(self):
         pot = PotFactory.create()
