093c04d18653f89a29a0cef5692ba6e3757c1e47
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-03 22:37:13 -1000

    Add simple create pot view tests

--- potluck/pots/tests/test_views.py
+++ potluck/pots/tests/test_views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.test import Client
 import pytest
 
+from potluck.pots.models import Pot
 from potluck.pots.tests.factories import PotFactory
 
 
@@ -16,7 +17,7 @@ class TestPotListView:
 
         assert response.status_code == HTTPStatus.OK
 
-    def test_pot_list_contains_existing_pot(self):
+    def test_pot_list_contains_existing_pot_name(self):
         pot = PotFactory.create()
         url = reverse("pots_list")
         client = Client()
@@ -38,6 +39,27 @@ class TestPotDetailView:
 
         assert response.status_code == HTTPStatus.OK
         assert pot.name in str(response.content)
+
+
+@pytest.mark.django_db
+class TestPotCreateView:
+    def test_get_sucess(self):
+        url = reverse("pot_create")
+        client = Client()
+
+        response = client.get(url)
+
+        assert response.status_code == HTTPStatus.OK
+
+    def test_post_creates_pot(self):
+        url = reverse("pot_create")
+        client = Client()
+        data = {"name": "Test Pot"}
+
+        response = client.post(url, data=data, follow=True)
+
+        assert response.status_code == HTTPStatus.OK
+        assert Pot.objects.first().name == "Test Pot"
 
 
 @pytest.mark.django_db
