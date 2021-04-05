7230c2799698c4ffa296fcb713d1fea1cf0eb305
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-04 22:43:36 -1000

    Test game titles in pick create view

--- potluck/picks/tests/test_views.py
+++ potluck/picks/tests/test_views.py
@@ -5,6 +5,7 @@ from django import urls
 import pytest
 
 from potluck.pots.tests.factories import PotFactory
+from potluck.games.tests.factories import GameFactory
 
 
 @pytest.mark.django_db
@@ -17,3 +18,15 @@ class TestPickCreateView:
         response = client.get(url)
 
         assert response.status_code == http.HTTPStatus.OK
+
+    def test_get_display_game_titles(self):
+        pot = PotFactory.create()
+        game_1 = GameFactory.create_with_teams(pot=pot)
+        game_2 = GameFactory.create_with_teams(pot=pot)
+        url = urls.reverse("pick_create", kwargs={"pot_id": pot.id})
+        client = test.Client()
+
+        response = client.get(url)
+
+        assert str(game_1) in str(response.content)
+        assert str(game_2) in str(response.content)
