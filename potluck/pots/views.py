28bb53cc99280728a77d3645efe0dd10d43e1a13
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-09 22:54:39 -1000

    Add pick create link on pot detail page

--- potluck/pots/views.py
+++ potluck/pots/views.py
@@ -13,6 +13,14 @@ class PotDetailView(generic.DetailView):
     model = Pot
     context_object_name = "pot"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+
+        picks_url = reverse_lazy("pick_create", kwargs={"pot_id": self.object.id })
+        context["picks_url"] = self.request.build_absolute_uri(picks_url)
+
+        return context
+
 
 class PotCreateView(generic.CreateView):
     model = Pot
