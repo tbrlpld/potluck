ea896496df20036055af21f845863c64de16d3c7
Tibor Leupold <tibor.leupold@torchbox.com>
2021-04-23 17:09:58 -1000

    Simplify PotFactory with RelatedFactoryList
    This is a feature directly build into factory boy. This totally removes the
    need for the whole post_generation hook in this case.
    
    Also, making the test much more specific (and making more sense too) removed
    the need for the method to create a pot without games.

--- potluck/games/tests/test_views.py
+++ potluck/games/tests/test_views.py
@@ -7,19 +7,17 @@ import pytest
 
 from potluck.games.models import Game
 from potluck.games.tests.factories import GameFactory
-from potluck.pots.tests.factories import PotFactory
 
 
 @pytest.mark.django_db
 class TestGameDeleteView:
     def test_post_deletes_game(self):
-        pot = PotFactory.create_without_games()
-        game = GameFactory.create(pot=pot)
+        game = GameFactory.create()
         url = reverse("game_delete", kwargs={"pk": game.id})
         client = Client()
-        assert Game.objects.count() == 1
+        assert game in Game.objects.all()
 
         response = client.post(url, data={}, follow=True)
 
         assert response.status_code == HTTPStatus.OK
-        assert Game.objects.count() == 0
+        assert game not in Game.objects.all()
--- potluck/pots/tests/factories.py
+++ potluck/pots/tests/factories.py
@@ -1,6 +1,5 @@
 import factory
 
-from potluck.games.tests.factories import GameFactory
 from potluck.pots.models import Pot
 
 
@@ -10,24 +9,8 @@ class PotFactory(factory.django.DjangoModelFactory):
 
     name = factory.Sequence(lambda n: f"Test Pot {n}")
 
-    @factory.post_generation
-    def games(self, create, extracted, **kwargs):
-        # If not called with the create method, do nothing.
-        if not create:
-            return
-
-        # If a list was passed into the create method, use that
-        if extracted:
-            for game in extracted:
-                game.pot = self
-                game.save()
-        # If noting was passed, create and add two teams to the game.
-        else:
-            GameFactory.create(pot=self)
-            GameFactory.create(pot=self)
-
-    @classmethod
-    def create_without_games(cls):
-        obj = cls.build()
-        obj.save()
-        return obj
+    games = factory.RelatedFactoryList(
+        "potluck.games.tests.factories.GameFactory",
+        factory_related_name="pot",
+        size=2,
+    )
