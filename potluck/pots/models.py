fe34b4e5baa251e34299de157d0e79f121056b90
Tibor Leupold <tibor.leupold@torchbox.com>
2021-06-19 14:42:57 -0700

    Add pot statuses

--- potluck/pots/models.py
+++ potluck/pots/models.py
@@ -4,6 +4,18 @@ from django.db import models
 class Pot(models.Model):
     name = models.CharField(max_length=250, null=False, blank=False)
 
+    class Status(models.TextChoices):
+        DRAFT = "DR", "Draft"
+        OPEN = "OP", "Open"
+        CLOSED = "CL", "Closed"
+        TALLY = "TA", "Tally"
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
+
     def __str__(self):
         return self.name
 
