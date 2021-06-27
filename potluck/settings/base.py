5bfaaa17873048c86c5e216d9ae1fea315eb427e
Tibor Leupold <tibor@lpld.io>
2021-06-26 21:37:54 -0700

    Add basic tailwind config

--- potluck/settings/base.py
+++ potluck/settings/base.py
@@ -143,6 +143,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [BASE_DIR / "potluck/static/dist"]
 
 
 # SECURITY
