818140d50e22d5ab502ed3178d9760141421df78
Tibor Leupold <tibor@lpld.io>
2021-07-05 15:07:00 -0700

    Make the template filter even more general
    Now the template filter is just used to insert into strings.
    
    Everything else (like what is inserted and where) needs to be handled
    in the outer context.

--- potluck/utils/templatetags/utils.py
+++ potluck/utils/templatetags/utils.py
@@ -1,9 +1,24 @@
 from django import template
-from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.filter
-def inject_class(value, attr):
-    return mark_safe(value[:-1] + f' class="{attr}">')
+def insert(value, args):
+    """
+    Insert a string into the value.
+
+    This filter inserts a given string into the value at a given index.
+
+    The index and the string need to be passed as one string as comma separated
+    arguments. E.g. to insert the string "example" at the second index (1) use like
+    this:
+
+    ```django+html
+    {{ value|insert:'0, example' }}
+    ```
+
+    """
+    index, insert_string = args.split(",")
+    index = int(index)
+    return value[:index] + insert_string + value[index:]
