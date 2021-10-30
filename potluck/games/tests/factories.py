6f07e3eebff868ee523fb0b9b57a7549ac83eba0
Tibor Leupold <tibor@lpld.io>
2021-10-30 14:19:05 -0700

    Remove reliance on subfactories from pick sheet view tests
    The subfactories make the tests hard to read, because they hide a bunch
    of complexity. It's probably better to encapsulate repeated setups in
    fixtures. These have to be explicitly called, which makes it more
    explicit than the subfactories that just run.

--- potluck/games/tests/factories.py
+++ potluck/games/tests/factories.py
@@ -9,19 +9,3 @@ class GameFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Game
 
-    pot = factory.SubFactory(PotFactory)
-
-    @factory.post_generation
-    def teams(self, create, extracted, **kwargs):
-        # If not called with the create method, do nothing.
-        if not create:
-            return
-
-        # If a list was passed into the create method, use that
-        if extracted:
-            for team in extracted:
-                self.teams.add(team)
-        # If noting was passed, create and add two teams to the game.
-        else:
-            self.teams.add(TeamFactory.create())
-            self.teams.add(TeamFactory.create())
