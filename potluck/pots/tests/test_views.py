94013e26514bc298a456ee55f66e6b1c7ca06e8d
Tibor Leupold <tibor@lpld.io>
2021-11-12 21:53:17 -0800

    Fix pot create view test

--- potluck/pots/tests/test_views.py
+++ potluck/pots/tests/test_views.py
@@ -69,7 +69,10 @@ class TestPotCreateView:
     def test_post_creates_pot(self):
         url = reverse("pot_create")
         client = Client()
-        data = {"name": "Test Pot"}
+        data = {
+            "name": "Test Pot",
+            "tiebreaker_description": "Tiebreker description"
+        }
 
         response = client.post(url, data=data, follow=True)
 
