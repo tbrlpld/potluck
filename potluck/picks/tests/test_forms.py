eeb5d12852ccd9c3b591c0ea08eaee295e2a1eae
Tibor Leupold <tibor@lpld.io>
2021-11-21 19:51:46 -0800

    Add more CreatePick form tests

--- potluck/picks/tests/test_forms.py
+++ potluck/picks/tests/test_forms.py
@@ -34,16 +34,46 @@ class TestCreatePickSheet:
 
 @pytest.mark.django_db
 class TestCreatePick:
-    def test_create_empty(self):
+
+    @pytest.fixture
+    def setup(self):
+        self.team_1 = teams_factories.TeamFactory.create()
+        self.team_2 = teams_factories.TeamFactory.create()
+        self.game = games_factories.GameFactory.create()
+        self.game.teams.set((self.team_1, self.team_2))
+
+    def test_empty(self):
         with pytest.raises(ValueError):
             form = forms.CreatePick()
 
-    def test_with_initial(self):
-        team_1 = teams_factories.TeamFactory.create()
-        team_2 = teams_factories.TeamFactory.create()
-        game = games_factories.GameFactory.create()
-        game.teams.set((team_1, team_2))
-
-        form = forms.CreatePick(initial={"game": game})
+    def test_with_game(self, setup):
+        form = forms.CreatePick(initial={"game": self.game})
 
         assert form.is_valid() is False
+
+    def test_with_data(self, setup):
+        form = forms.CreatePick(
+            data={
+                "game": self.game,
+                "picked_team": self.team_1,
+            },
+            initial={"game": self.game},
+        )
+
+        assert form.is_valid() is True
+        pick = form.save(commit=False)
+        assert pick.game == self.game
+        assert pick.picked_team == self.team_1
+
+    def test_with_team_not_from_game(self, setup):
+        team_3 = teams_factories.TeamFactory.create()
+        form = forms.CreatePick(
+            data={
+                "game": self.game,
+                "picked_team": team_3,
+            },
+            initial={"game": self.game},
+        )
+
+        assert form.is_valid() is False
+        assert "picked_team" in form.errors
