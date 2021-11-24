c802d9a3f7dbbc6e9553f011f31ede0c31854eff
Tibor Leupold <tibor@lpld.io>
2021-11-24 10:35:23 -0800

    Remove default secret key from settings file
    There should not be a default value for this secutiry critial value
    because that makes it possible that a value that is in the repo is being
    used in production.
    
    The docker compose file does set a secret key for use in dev and test.

--- potluck/settings/base.py
+++ potluck/settings/base.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "this-is-not-secret")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", False))
