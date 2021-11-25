103ae2cb7c5e67a4969c94ae39a21db626a8827b
Tibor Leupold <tibor@lpld.io>
2021-11-25 11:29:15 -0800

    Use module level import style

--- potluck/games/tests/test_models.py
+++ potluck/games/tests/test_models.py
@@ -2,28 +2,28 @@ from django.core import exceptions
 
 import pytest
 
-from potluck.games.models import Game
-from potluck.games.tests.factories import GameFactory
-from potluck.teams.tests.factories import TeamFactory
+from potluck.games import models as games_models
+from potluck.games.tests import factories as games_factories
+from potluck.teams.tests import factories as teams_factories
 
 
 @pytest.mark.django_db
 class TestGame:
     def test_set_winning_team_saves_to_db(self):
-        team_1 = TeamFactory.create()
-        team_2 = TeamFactory.create()
-        game = GameFactory.create()
+        team_1 = teams_factories.TeamFactory.create()
+        team_2 = teams_factories.TeamFactory.create()
+        game = games_factories.GameFactory.create()
         game.teams.set((team_1, team_2))
 
         game.set_winning_team(team_1)
 
-        assert Game.objects.get(pk=game.id).winning_team == team_1
+        assert games_models.Game.objects.get(pk=game.id).winning_team == team_1
 
     def test_set_winning_team_with_team_not_in_game(self):
-        team_1 = TeamFactory.create()
-        team_2 = TeamFactory.create()
-        team_not_in_game = TeamFactory.create()
-        game = GameFactory.create()
+        team_1 = teams_factories.TeamFactory.create()
+        team_2 = teams_factories.TeamFactory.create()
+        team_not_in_game = teams_factories.TeamFactory.create()
+        game = games_factories.GameFactory.create()
         game.teams.set((team_1, team_2))
         assert team_not_in_game not in game.teams.all()
 
