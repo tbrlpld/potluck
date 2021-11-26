f699d2d9219bb6933f158a44008b2a84c8020849
Tibor Leupold <tibor@lpld.io>
2021-11-25 20:18:53 -0800

    Add test for `is_tie` field

--- potluck/games/tests/test_models.py
+++ potluck/games/tests/test_models.py
@@ -9,23 +9,28 @@ from potluck.teams.tests import factories as teams_factories
 
 @pytest.mark.django_db
 class TestGame:
-    def test_set_winning_team_saves_to_db(self):
-        team_1 = teams_factories.TeamFactory.create()
-        team_2 = teams_factories.TeamFactory.create()
-        game = games_factories.GameFactory.create()
-        game.teams.set((team_1, team_2))
+    @pytest.fixture
+    def setup(self):
+        self.team_1 = teams_factories.TeamFactory.create()
+        self.team_2 = teams_factories.TeamFactory.create()
+        self.game = games_factories.GameFactory.create()
+        self.game.teams.set((self.team_1, self.team_2))
 
-        game.set_winning_team(team_1)
+    def test_set_winning_team_saves_to_db(self, setup):
+        self.game.set_winning_team(self.team_1)
 
-        assert games_models.Game.objects.get(pk=game.id).winning_team == team_1
+        assert (
+            games_models.Game.objects.get(pk=self.game.id).winning_team == self.team_1
+        )
 
-    def test_set_winning_team_with_team_not_in_game(self):
-        team_1 = teams_factories.TeamFactory.create()
-        team_2 = teams_factories.TeamFactory.create()
+    def test_set_winning_team_with_team_not_in_game(self, setup):
         team_not_in_game = teams_factories.TeamFactory.create()
-        game = games_factories.GameFactory.create()
-        game.teams.set((team_1, team_2))
-        assert team_not_in_game not in game.teams.all()
+        assert team_not_in_game not in self.game.teams.all()
 
         with pytest.raises(exceptions.ValidationError):
-            game.set_winning_team(team_not_in_game)
+            self.game.set_winning_team(team_not_in_game)
+
+    def test_is_tie(self):
+        game = games_factories.GameFactory(is_tie=True)
+
+        assert game.is_tie == True
