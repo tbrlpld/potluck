819410d4eedfd8d56591f92c3bd72fae6994fab9
Tibor Leupold <tibor@lpld.io>
2021-12-04 15:26:07 -0800

    Add with_pot trait to game factory

--- potluck/games/factories.py
+++ potluck/games/factories.py
@@ -1,6 +1,7 @@
 import factory
 
 from potluck.games.models import Game
+from potluck.pots.tests import factories as pots_factories
 from potluck.teams.tests import factories as teams_factories
 
 
@@ -13,3 +14,4 @@ class Game(factory.django.DjangoModelFactory):
             home_team=factory.SubFactory(teams_factories.TeamFactory),
             away_team=factory.SubFactory(teams_factories.TeamFactory),
         )
+        with_pot = factory.Trait(pot=factory.SubFactory(pots_factories.PotFactory))
