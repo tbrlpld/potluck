7d0c01862b22badce36d933444cba7d48517f651
Tibor Leupold <tibor@lpld.io>
2021-12-04 15:23:52 -0800

    Test games factory with_pot trait

--- potluck/games/tests/test_factories.py
+++ potluck/games/tests/test_factories.py
@@ -1,15 +1,21 @@
 from potluck.games import factories as games_factories
+from potluck.pots import models as pots_models
 from potluck.teams import models as teams_models
 
 
 class TestGame:
-    def test_init(self):
+    def test_build(self):
         game = games_factories.Game.build()
 
         assert True
 
-    def test_with_teams_trait(self):
+    def test_build_with_teams_trait(self):
         game = games_factories.Game.build(with_teams=True)
 
         assert isinstance(game.home_team, teams_models.Team)
         assert isinstance(game.away_team, teams_models.Team)
+
+    def test_build_with_pot_trait(self):
+        game = games_factories.Game.build(with_pot=True)
+
+        assert isinstance(game.pot, pots_models.Pot)
