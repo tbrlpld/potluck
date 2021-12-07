033ba43c3415cc0b383ab0b523c6c7a0b822c1bd
Tibor Leupold <tibor@lpld.io>
2021-12-06 19:21:45 -0800

    Create basic setup for pattern library

--- potluck/settings.py
+++ potluck/settings.py
@@ -108,18 +108,18 @@ if DEBUG:
         # are the group titles and the values are lists of template name
         # prefixes that will be searched to populate the groups.
         "SECTIONS": (
-            ("components", ["patterns/components"]),
-            ("pages", ["patterns/pages"]),
+            ("atoms", ["atoms"]),
+            ("pages", ["pages"]),
         ),
         # Configure which files to detect as templates.
         "TEMPLATE_SUFFIX": ".html",
         # Set which template components should be rendered inside of,
         # so they may use page-level component dependencies like CSS.
-        "PATTERN_BASE_TEMPLATE_NAME": "patterns/base.html",
+        "PATTERN_BASE_TEMPLATE_NAME": "base.html",
         # Any template in BASE_TEMPLATE_NAMES or any template that
         # extends a template in BASE_TEMPLATE_NAMES is a "page" and will be
         # rendered as-is without being wrapped.
-        "BASE_TEMPLATE_NAMES": ["patterns/base_page.html"],
+        "BASE_TEMPLATE_NAMES": ["base_page.html"],
     }
 
 WSGI_APPLICATION = "potluck.wsgi.application"
